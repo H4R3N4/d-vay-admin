354ac7b24c31d3e25ce46cdce20854398b4e4685
Harena <arenanofidina@gmail.com>
2024-04-13 15:39:41 +0300

    commit before pull

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -23,6 +23,7 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
             <Link href="/" className="layout-topbar-logo">
                 <img src={`/layout/images/logo-${layoutConfig.colorScheme !== 'light' ? 'white' : 'dark'}.svg`} width="47.22px" height={'35px'} alt="logo" />
                 <span>SAKAI</span>
+                {/* <img src="D:\andriamifidy\NOFIDIANA\FIANARANA\ENI\PROJET IHM\d-vay-admin\public\image\logo_d-vayRectangle.png" alt="logo" /> */}
             </Link>
 
             <button ref={menubuttonRef} type="button" className="p-link layout-menu-button layout-topbar-button" onClick={onMenuToggle}>
